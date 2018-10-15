81cdcee0faa75235d71242ff74d7cd04986b81f9
Dmitry Kryukov <remasik@gmail.com>
2018-10-14 20:27:00 -0400

    Fix size of map panel

--- src/game/ui/Main.java
+++ src/game/ui/Main.java
@@ -28,7 +28,6 @@ public class Main {
         //frame.setDefaultLookAndFeelDecorated(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-
         // Top Status Game Info Bar
         TopStatusPanel topStatusPanel = new TopStatusPanel(width, 30);
         //topStatusPanel.setBorder(new LineBorder(Color.BLUE, 1));
@@ -54,7 +53,7 @@ public class Main {
         // Left Panel
         // Map Panel Map
         //TODO: Extract Logic and reshuffle top to bottom left to right.
-        MapPanel mapPanel = new MapPanel(new Dimension(935, height), game.getCountries(), game.neighbours, topStatusPanel, rightStatusPanel);
+        MapPanel mapPanel = new MapPanel(new Dimension(950, height), game.getCountries(), game.neighbours, topStatusPanel, rightStatusPanel);
         //mapPanel.setBorder(new LineBorder(Color.BLACK, 4));
         mapPanel.setBackground(new Color(119,178,140));
 
