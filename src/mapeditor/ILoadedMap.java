9ee6b2139418da545b8a04d8ad978a0daa808a70
Rodolfo Miranda <rodolfomotamiranda@gmail.com>
2018-10-19 23:04:18 -0400

    add method for the interface

--- src/mapeditor/ILoadedMap.java
+++ src/mapeditor/ILoadedMap.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 /**
  * This class contains data structures into which the map is loaded.
- * @author Nikitha Papani & Dinesh Pattapu
+ * @author Nikitha Papani & Dinesh Pattapu & Rodolfo Miranda
  *
  */
 public interface ILoadedMap {
@@ -20,7 +20,9 @@ public interface ILoadedMap {
 	public IContinent getContinent(String continentName);
 	public ITerritory getTerritory(String territoryName);
 	public ArrayList<String> getListOfContinents();
+        public ArrayList<IContinent> getContinents();
 	public ArrayList<String> getListOfTerritories();
+        public ArrayList<ITerritory> getTerritories();
 	public boolean saveMapToFile(String path) throws FileNotFoundException, UnsupportedEncodingException;
 	public void handlecontinentnamechange(String oldName, String newName, IContinent continent);
 	public void handleterritorynamechange(String oldName, String newName, ITerritory territory);
