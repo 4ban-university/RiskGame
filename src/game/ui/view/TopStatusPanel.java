8da55b7a3e7f969b3cd2cfaf480f2a54e5da1971
Ksenia Popova <p.ksyusha@gmail.com>
2018-10-29 22:20:20 -0400

    Panels for observer pattern

--- src/game/ui/view/TopStatusPanel.java
+++ src/game/ui/view/TopStatusPanel.java
@@ -1,5 +1,7 @@
 package game.ui.view;
 
+import game.Game;
+import game.model.IModelObservable;
 import game.model.Player;
 
 import javax.swing.*;
@@ -10,10 +12,14 @@ import java.awt.*;
  * @author Dmitry Kryukov
  * @see Player
  */
-public class TopStatusPanel extends JPanel {
-    private JLabel playerColor = new JLabel();
+public class TopStatusPanel extends JPanel implements IPanelObserver {
+
+    private Player player;
+    private String turnPhraseText;
+    private String gamePhaseText;
+
+
     private JLabel playerName = new JLabel();
-//    private JLabel gameState = new JLabel();
     private JLabel gamePhase = new JLabel();
     private JLabel turnPhrase = new JLabel();
 
@@ -34,13 +40,6 @@ public class TopStatusPanel extends JPanel {
         separator1.setForeground(new Color(121,180,115)); // make it invisible
         this.add(separator1);
 
-//        this.add(new JLabel("State: "));
-//        this.add(gameState);
-//        JSeparator separator2 = new JSeparator(SwingConstants.HORIZONTAL); // Horizontal separator
-//        separator2.setBackground(new Color(121,180,115)); // make it invisible
-//        separator2.setForeground(new Color(121,180,115)); // make it invisible
-//        this.add(separator2);
-
         this.add(new JLabel("Status: "));
         this.add(turnPhrase);
         JSeparator separator3 = new JSeparator(SwingConstants.HORIZONTAL); // Horizontal separator
@@ -50,45 +49,60 @@ public class TopStatusPanel extends JPanel {
 
         this.add(new JLabel("Phase: "));
         this.add(gamePhase);
+
+        Game.getInstance().attachObserver(this);
     }
 
-    /**
-     * Set the player info on the pstatus panel
-     * @param player Player
-     */
-    public void setPlayer(Player player) {
-        playerName.setText(player.getName());
-        String colorS = Integer.toString(player.getColor().getRGB());
-        playerName.setForeground(new Color(Integer.parseInt(colorS)));
+    @Override
+    public void updateObserver(IModelObservable iModelObservable) {
+        Game game = Game.getInstance();
+        playerName.setText(game.getCurrentPlayer() != null ? game.getCurrentPlayer().getName() : "");
+        gamePhase.setText(game.getCurrentGamePhase() != null ? game.getCurrentGamePhase().getName() : "");
+        turnPhrase.setText(game.getCurrentTurnPhraseText() != null ? game.getCurrentTurnPhraseText() : "");
+//        this.repaint();
+
+//        switch (iModelObservable.getClass().getName()) {
+//            case "Player":
+//                player = (Player) iModelObservable;
+//                break;
+//            case "Game":
+//                gamePhaseText = ((Game) iModelObservable).getCurrentGamePhase().getName();
+//                break;
+//        }
     }
 
-//    public void setGameState(String gameState) {
-//        this.gameState.setText(gameState);
+//    /**
+//     * Set the player info on the pstatus panel
+//     * @param player Player
+//     */
+//    public void setPlayer(Player player) {
+//        playerName.setText(player.getName());
+//        String colorS = Integer.toString(player.getColor().getRGB());
+//        playerName.setForeground(new Color(Integer.parseInt(colorS)));
 //    }
-
-    /**
-     * Set the current game phase to displaying on the panel
-     * @param gamePhase phase
-     */
-    public void setGamePhase(String gamePhase) {
-        this.gamePhase.setText(gamePhase);
-    }
-
-    /**
-     * Set the status for the phase to displaying on the panel
-     * @param turnPhrase Phrase
-     */
-    public void setTurnPhrase(String turnPhrase) {
-        this.turnPhrase.setText(turnPhrase);
-    }
-
-    /**
-     * Reset the variables for other players
-     */
-    public void reset() {
-        playerName.setText("");
-//        gameState.setText("");
-        gamePhase.setText("");
-        turnPhrase.setText("");
-    }
+//
+//    /**
+//     * Set the current game phase to displaying on the panel
+//     * @param gamePhase phase
+//     */
+//    public void setGamePhase(String gamePhase) {
+//        this.gamePhase.setText(gamePhase);
+//    }
+//
+//    /**
+//     * Set the status for the phase to displaying on the panel
+//     * @param turnPhrase Phrase
+//     */
+//    public void setTurnPhrase(String turnPhrase) {
+//        this.turnPhrase.setText(turnPhrase);
+//    }
+//
+//    /**
+//     * Reset the variables for other players
+//     */
+//    public void reset() {
+//        playerName.setText("");
+//        gamePhase.setText("");
+//        turnPhrase.setText("");
+//    }
 }
