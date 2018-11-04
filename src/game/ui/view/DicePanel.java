728f7308d46b273a8b4ad8b4b03f4d29f5f4cc3d
Ksenia Popova <p.ksyusha@gmail.com>
2018-11-04 17:54:08 -0500

    Refactor Dice panel

--- src/game/ui/view/DicePanel.java
+++ src/game/ui/view/DicePanel.java
@@ -1,8 +1,13 @@
 package game.ui.view;
 
+import game.Game;
 import game.enums.DiceEnum;
+import game.model.Dice;
+import game.model.IModelObservable;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -12,13 +17,10 @@ import java.io.IOException;
  * Dice panel.
  * Stub for future development.
  */
-public class DicePanel extends JPanel {
+public class DicePanel extends JPanel implements IPanelObserver {
     public final int DICE_ROW_TO_SHOW = 3;
-    private final int DICE_MARGIN = 5;
     private DiceEnum[] redDice = new DiceEnum[DICE_ROW_TO_SHOW];
     private DiceEnum[] whiteDice = new DiceEnum[DICE_ROW_TO_SHOW];
-    private int width;
-    private int height;
 
     /**
      * Constructor of the class
@@ -26,70 +28,41 @@ public class DicePanel extends JPanel {
      * @param height of the panel
      */
     public DicePanel(int width, int height) {
-        this.width = width;
-        this.height = height;
         this.setPreferredSize(new Dimension(width, height));
         this.setLayout(new GridLayout(3, 2));
-        for (int i = 0; i < DICE_ROW_TO_SHOW; i++) {
-            redDice[i] = DiceEnum.SIX;
-            whiteDice[i] = DiceEnum.SIX;
-        }
-        setDices();
+        Game.getInstance().attachObserver(this);
     }
 
-    /**
-     * Set the dices
-     */
-    public void setDices() {
-        setDices(redDice, whiteDice);
+    @Override
+    public void updateObserver(IModelObservable iModelObservable) {
+        Game game = Game.getInstance();
+        whiteDice = game.getWhiteDice();
+        redDice = game.getRedDice();
+        createPanel();
     }
 
-    /**
-     * Set the dices with parameters
-     * @param redDice Dices of player
-     * @param whiteDice Dices of enemie
-     */
-    public void setDices(DiceEnum[] redDice, DiceEnum[] whiteDice) {
-        this.redDice = redDice;
-        this.whiteDice = whiteDice;
+    private void createPanel() {
         this.removeAll();
+
         for (int i = 0; i < DICE_ROW_TO_SHOW; i++) {
             try {
-                BufferedImage imageRed = ImageIO.read(new File(DiceEnum.RED_FILE_PREFIX + redDice[i].getFileSuffix()));
-                BufferedImage imageWhite = ImageIO.read(new File(DiceEnum.WHITE_FILE_PREFIX + whiteDice[i].getFileSuffix()));
+                BufferedImage imageRed = ImageIO.read(new File(Dice.RED_FILE_PREFIX + redDice[i].getFileSuffix()));
+                BufferedImage imageWhite = ImageIO.read(new File(Dice.WHITE_FILE_PREFIX + whiteDice[i].getFileSuffix()));
                 JLabel redLabel = new JLabel();
                 redLabel.setIcon(new ImageIcon(imageRed));
                 redLabel.setHorizontalAlignment(JLabel.CENTER);
                 redLabel.setVerticalAlignment(JLabel.CENTER);
+                redLabel.setBorder(new LineBorder(Color.BLACK));
                 this.add(redLabel);
                 JLabel whiteLabel = new JLabel();
                 whiteLabel.setIcon(new ImageIcon(imageWhite));
                 whiteLabel.setHorizontalAlignment(JLabel.CENTER);
                 whiteLabel.setVerticalAlignment(JLabel.CENTER);
+                whiteLabel.setBorder(new LineBorder(Color.BLACK));
                 this.add(whiteLabel);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-//        for(int i = 0; i < DICE_ROW_TO_SHOW; i++){
-//            diceRows[i] = new JPanel();
-//            diceRows[i].add(new ImageIcon("ressources/" + "white_" + whiteDice[i].getFileSuffix()));
-//            diceRows[i].add(new ImageIcon("ressources/" + "red_"+ redDice[i].getFileSuffix()));
-//        }
     }
-    //    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//        for (int i = 0; i < DICE_ROW_TO_SHOW; i++) {
-//            try {
-//                File imageRed = new File("game.resources/red_" + redDice[0].getFileSuffix());
-//                g.drawImage(ImageIO.read(imageRed).getScaledInstance(width / 2, height / 3, Image.SCALE_SMOOTH), 0, 0 + i * (height - DICE_MARGIN) / 3, this);
-//
-//                File whiteRed = new File("game.resources/white_" + redDice[0].getFileSuffix());
-//                g.drawImage(ImageIO.read(whiteRed).getScaledInstance(width / 2, height / 3, Image.SCALE_SMOOTH), 0 + (width - DICE_MARGIN) / 2, 0 + i * (height - DICE_MARGIN) / 3, this);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 }
