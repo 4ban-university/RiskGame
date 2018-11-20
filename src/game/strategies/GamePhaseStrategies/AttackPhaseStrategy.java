9a500bfd182ccc7bd747fb00d88b21ab7e4dff2b
Dmitry Kryukov <remasik@gmail.com>
2018-11-20 12:55:24 -0500

    Refactor strategies

--- src/game/strategies/GamePhaseStrategies/AttackPhaseStrategy.java
+++ src/game/strategies/GamePhaseStrategies/AttackPhaseStrategy.java
@@ -8,14 +8,11 @@ import game.model.enums.CardsEnum;
 import java.util.Map;
 import java.util.Random;
 
-import static game.model.enums.CardsEnum.ARTILLERY;
-import static game.model.enums.CardsEnum.CAVALRY;
-import static game.model.enums.CardsEnum.INFANTRY;
-import static game.model.enums.CardsEnum.WILDCARDS;
+import static game.model.enums.CardsEnum.*;
 import static game.strategies.GamePhaseStrategies.GamePhaseEnum.ATTACK;
 import static game.strategies.GamePhaseStrategies.GamePhaseEnum.FORTIFICATION;
 
-public class AttackPhaseStrategy extends AbstractGamePhaseStrategy {
+public class AttackPhaseStrategy extends BasePhaseStrategy {
 
     /**
      * Check if player can attack anybody or go to next turn
